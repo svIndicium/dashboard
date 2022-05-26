a42f504ee42c1b303ced0120ce7884edd80a5e42
joostlek <joostlek@outlook.com>
2022-05-26 14:00:17 +0200

    Fix CI/CD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { NuxtConfig } from "@nuxt/types"
+import colors from "vuetify/lib/util/colors"
 
 const config: Omit<NuxtConfig, "env"> = {
   // Target: https://go.nuxtjs.dev/config-target
@@ -67,20 +68,20 @@ const config: Omit<NuxtConfig, "env"> = {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    treeShake: true,
     theme: {
-      disable: true
-      // dark: false,
-      // themes: {
-      //   dark: {
-      //     primary: colors.blue.darken2,
-      //     accent: colors.grey.darken3,
-      //     secondary: colors.amber.darken3,
-      //     info: colors.teal.lighten1,
-      //     warning: colors.amber.base,
-      //     error: colors.deepOrange.accent4,
-      //     success: colors.green.accent3
-      //   }
-      // }
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
     }
   },
 
